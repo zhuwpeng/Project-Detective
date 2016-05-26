338d760878e9fa9bfe9a620bc1e6168be7799811
zhuwpeng <zhuwpeng@gmail.com>
2016-05-26 17:43:09 +0200

    No changes
    Just removed comment

--- character.php
+++ character.php
@@ -4,9 +4,9 @@ class Character {
 	
 	public $name;
 	
-// 	public $strength;
-// 	public $intelligence;
-// 	public $charisma;
+ 	public $strength;
+ 	public $intelligence;
+ 	public $charisma;
 	
 	public function __construct($name) {
 		$this->name = $name;
