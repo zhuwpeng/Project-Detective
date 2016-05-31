7067199664595dfe27ff5640f50a5d361111ffc6
zhuwpeng <zhuwpeng@gmail.com>
2016-05-31 21:48:32 +0200

    Basic character class
    - Created a basic character class as base for the detective and suspect

--- character.php
+++ character.php
@@ -7,12 +7,38 @@ class Character {
  	public $strength;
  	public $intelligence;
  	public $charisma;
+ 	public $gender;
+ 	public $birthdate;
 	
-	public function __construct($name) {
+ 	//Constructor
+	public function __construct($name, $strength, $intelligence, $charisma, $gender, $birthdate) {
 		$this->name = $name;
+		$this->strength = $strength;
+		$this->intelligence = $intelligence;
+		$this->charisma = $charisma;
+		$this->birthdate = $birthdate;
+		$this->gender = $gender;
+	}
+	
+	//Display character stats
+	public function display(){
+		$age = $this->calculateAge($this->birthdate);
+		print "Name: " . $this->name . "<br>";
+		print "Age: " . $age . "<br>";
+		print "Strength: " . $this->strength . "<br>";
+		print "Intelligence: " . $this->intelligence . "<br>";
+		print "Charisma: " . $this->charisma . "<br>";
+		print "Gender: " . $this->gender . "<br>";
 	}
 	
 	public function getName() {
 		return $this->name;
+	}
+	
+	public function calculateAge($birthday){
+		$born = new DateTime("$birthday");
+		$now = new DateTime("now");
+		$age = $born->diff($now)->y;
+		return $age;
 	}
 }
